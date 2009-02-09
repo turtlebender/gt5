6b8e2b7714c663ca5ecba21b5e098fdafab6954e
Tom Howe <trhowe@uchicago.edu>
2009-02-09 14:54:47 -0600

    moved the creation of the jaxb factory into my resource factory.  this allows me to set the factory fields.  later I'll need to make this support more than just jaxb, but it works for now

--- spring-ws/src/main/java/org/globus/resourceful/springws/ResourcefulEndpointFactory.java
+++ spring-ws/src/main/java/org/globus/resourceful/springws/ResourcefulEndpointFactory.java
@@ -1,8 +1,8 @@
 package org.globus.resourceful.springws;
 
-import org.globus.wsrf.ResourceDelegateFactory;
 import org.globus.wsrf.BeanProcessor;
 import org.globus.wsrf.ProcessedResource;
+import org.globus.wsrf.ResourceDelegateFactory;
 import org.globus.wsrf.WebMethodInvoker;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.FatalBeanException;
@@ -10,12 +10,16 @@ import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.oxm.Marshaller;
 import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ResourcefulEndpointFactory implements FactoryBean, BeanPostProcessor {
     boolean resourcePopulated = false;
     private ResourcefulMethodEndpointAdapter adapter;
+    private String jaxbContextPath;
     private Marshaller marshaller;
     private Unmarshaller unmarshaller;
     private BeanProcessor processor = new BeanProcessor();
@@ -28,23 +32,43 @@ public class ResourcefulEndpointFactory implements FactoryBean, BeanPostProcesso
         return marshaller;
     }
 
-    public void setMarshaller(Marshaller marshaller) {
-        this.marshaller = marshaller;
-    }
 
     public Unmarshaller getUnmarshaller() {
         return unmarshaller;
     }
 
-    public void setUnmarshaller(Unmarshaller unmarshaller) {
-        this.unmarshaller = unmarshaller;
+    public void setJaxbContextPath(String jaxbContextPath) {
+        this.jaxbContextPath = jaxbContextPath;
     }
 
+    private void instantiateMarshallers(String jaxbContextPath) throws Exception{
+        String[] pathElements = jaxbContextPath.split(":");
+        Map<String, Boolean> factoryPaths = new HashMap<String, Boolean>();
+        factoryPaths.put("org.oasis.wsrf.resourceproperties", false);
+        factoryPaths.put("org.oasis.wsrf.lifetime", false);
+        for (String pathElement : pathElements) {
+            if (factoryPaths.containsKey(pathElement)) {
+                factoryPaths.put(pathElement, true);
+            }
+        }
+        StringBuilder builder = new StringBuilder(jaxbContextPath);
+        for (Map.Entry<String, Boolean> factoryPath : factoryPaths.entrySet()) {
+            if (!factoryPath.getValue()) {
+                builder.append(":");
+                builder.append(factoryPath.getKey());
+            }
+        }
+        Jaxb2Marshaller tmp = new Jaxb2Marshaller();
+        tmp.setContextPath(builder.toString());
+        tmp.afterPropertiesSet();
+        this.marshaller = tmp;
+        this.unmarshaller = tmp;
+    }
 
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        if(resourcePopulated){
+        if (resourcePopulated) {
             return bean;
-        }
+        }        
         ProcessedResource resource;
         try {
             resource = processor.invoke(bean);
@@ -53,12 +77,19 @@ public class ResourcefulEndpointFactory implements FactoryBean, BeanPostProcesso
         }
 
         if (resource != null) {
+            try {
+                instantiateMarshallers(jaxbContextPath);
+            } catch (Exception e) {
+                throw new FatalBeanException("Unable to create JAXB context", e);
+            }
             resourcePopulated = true;
             WebMethodInvoker invoker = new WebMethodInvoker();
             invoker.setUnmarshaller(this.unmarshaller);
             invoker.setMarshaller(this.marshaller);
             invoker.setXpression(resource.getEvaluator());
             adapter.setInvoker(invoker);
+            adapter.setMarshaller(this.marshaller);
+            adapter.setUnmarshaller(this.unmarshaller);
             return resource.getResource();
         }
         return bean;
